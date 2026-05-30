6a6e218beb1d4e11afb07f5638fc995f064b22d2
MCHPixel <schulzefabian03@outlook.de>
2026-05-30 08:48:38 +0200

    Initialized my Plugin!

---
+++ src/main/java/com/mchpixel/analy/Analy.java
@@ -0,0 +1,17 @@
+package com.mchpixel.analy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Analy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
